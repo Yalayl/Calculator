faedbb740ef51aa4eae6a63e90ae5185f8ec8213
Adrien Calmesnil <adcalmesnil@gmail.com>
2022-11-09 09:28:53 +0100

    test: calculator

--- js/calculator.js
+++ js/calculator.js
@@ -1,16 +1,16 @@
 class Calculator {
-  constructor (authorizer) {
-    this.authorizer = authorizer
+  constructor(authorizer) {
+    this.authorizer = authorizer;
   }
 
-  divide (numerator, denominator) {
-    const authorized = this.authorizer.authorize()
+  divide(numerator, denominator) {
+    const authorized = this.authorizer.authorize();
     // Bug! Sholud be `if(!authorized)`
     if (authorized) {
-      throw new Error('Not authorized')
+      throw new Error("Not authorized");
     }
-    return numerator / denominator
+    return numerator / denominator;
   }
 }
 
-module.exports = Calculator
+module.exports = Calculator;
--- js/tests/calculotor.test.js
+++ js/tests/calculotor.test.js
@@ -1,7 +1,19 @@
-const test = require('tape')
+const test = require("tape");
+const Calculator = require("../calculator");
+const Authorizer = require("../authorizer");
 
-test('should throw when not authorized', (t) => {
+test("should throw when not authorized", (t) => {
   // TODO: write a test that fails due to the bug in
-  // Calculator.divide()
-  t.end()
-})
+  //Given
+  const numerator = 12;
+  const denominator = 6;
+  const expected = 2;
+  const autorizer = new Authorizer();
+  jest.spyOn(autorizer, "authorize").mockReturnValue(true);
+  const calculator = new Calculator(autorizer);
+  //When
+  const result = calculator.divide(numerator, denominator);
+
+  //Then
+  jest.expect(result === expected);
+});
